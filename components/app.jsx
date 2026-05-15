25a64d463b25b6abfa65828478fcf1b5f11403d3
njshomesolutions <info@njshome.com>
2026-05-15 10:13:14 -0500

    Update app.jsx testimonios comentados

--- components/app.jsx
+++ components/app.jsx
@@ -20,7 +20,7 @@ function App() {
       <Services />
       <Gallery />
       <Process />
-      <Testimonials />
+      {/* <Testimonials /> */}
       <ServiceArea />
       <Contact />
       <Footer />
